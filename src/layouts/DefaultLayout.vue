11590ec749ecf64a863123be8d246ce0243e680c
Ivan Polennikov <lerner@MacBook-Pro-Ivan.local>
2023-10-12 14:25:13 +0300

    Refactor drawer

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,11 +1,22 @@
 <template>
   <v-app>
-    <v-navigation-drawer>
-      <v-list-item title="My Application" subtitle="Vuetify"></v-list-item>
+    <v-navigation-drawer color="black" width="300">
+      <div class="d-flex flex-column align-center py-4 mt-4">
+        <v-avatar size="100">
+          <v-img src="http://placekitten.com/300/300"  alt="User Avatar" />
+        </v-avatar>
+        <span class="mt-3">Your Username</span>
+        <span class="mt-3">Активна (43 дней)</span>
+      </div>
       <v-divider></v-divider>
-      <v-list-item link title="List Item 1"></v-list-item>
-      <v-list-item link title="List Item 2"></v-list-item>
-      <v-list-item link title="List Item 3"></v-list-item>
+      <v-list>
+        <v-list-item class="focusable" color="primary" link>
+          <template v-slot:prepend>
+            <v-icon icon="mdi-at"></v-icon>
+          </template>
+          <v-list-item-title>Home</v-list-item-title>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
     <v-main>
       <slot></slot>
